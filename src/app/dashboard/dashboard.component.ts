bc9c74c33618ec8705ed41b1337c1fcb137a6639
nardeep <nardeep.chaudhary@gmail.com>
2022-05-08 20:47:02 +1000

    Updated Readme.md

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  OnDestroy,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReplaySubject } from 'rxjs';
 import { OccupationRatings } from '../constants/occupation-ratings';
-import { Occupation } from '../models/occupation';
 import { PremiumCalculatorService } from '../services/premium-calculator.service';
 import { CustomValidators } from '../utils/custom-validators';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +18,7 @@ import { CustomValidators } from '../utils/custom-validators';
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   form!: FormGroup;
   monthlyPremium!: number;
   occupations = OccupationRatings;
@@ -20,8 +27,14 @@ export class DashboardComponent implements OnInit {
     year: new Date().getFullYear() - 2,
     month: 12,
     day: 31,
-  }
-  constructor(private fb: FormBuilder, private premiumCalculatorService: PremiumCalculatorService, private cdRef: ChangeDetectorRef) {}
+  };
+  destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  constructor(
+    private fb: FormBuilder,
+    private premiumCalculatorService: PremiumCalculatorService,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -32,27 +45,40 @@ export class DashboardComponent implements OnInit {
       deathSumInsured: ['', Validators.required],
     });
 
-    this.form.get('dateOfBirth')?.valueChanges.subscribe(dob => {
-      this.form.patchValue({'age': this.getAge(dob)});
-    })
+    this.form.get('dateOfBirth')?.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe((dob: any) => {
+      this.form.patchValue({ age: this.getAge(dob) });
+    });
+
+    this.form.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      this.calculate();
+    });
   }
 
-  calculate(){
-    this.submitted = true;
-    if(this.form.valid){
-      this.monthlyPremium = this.premiumCalculatorService.calculate(this.form.value.deathSumInsured, this.form.value.occupation, this.form.value.age);
+  calculate() {
+
+    if (this.form.valid) {
+      this.monthlyPremium = this.premiumCalculatorService.calculate(
+        this.form.value.deathSumInsured,
+        this.form.value.occupation,
+        this.form.value.age
+      );
     }
     this.cdRef.markForCheck();
   }
 
-  getAge(birthDate : {day : number, month: number, year: number}) {
+  getAge(birthDate: { day: number; month: number; year: number }) {
     var today = new Date();
     var age = today.getFullYear() - birthDate.year;
     var m = today.getMonth() - birthDate.month;
     if (m < 0 || (m === 0 && today.getDate() < birthDate.day)) {
-        age--;
+      age--;
     }
     return age;
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
 }
 
-}
